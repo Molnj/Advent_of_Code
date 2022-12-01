0dc745bee66a74e644c16d83574e322ee137b5c5
Mate <momatehun@gmail.com>
2022-12-01 06:37:20 +0100

    day01

--- 2022/Src/day01.py
+++ 2022/Src/day01.py
@@ -7,13 +7,38 @@ file_name = os.path.abspath(os.path.realpath(file_name))
 
 def read_file(file_name):
     with open(file_name, "r+") as file:
-        contents = file.read()
-    print(contents)
+        contents = file.readlines()
+    return contents
     
     
-def main():
-    read_file(file_name)
+def part1():
+    max_kcal = 0
+    curr_val = 0;
+    for line in read_file(file_name):
+        line = line.strip()
+        if len(line) != 0:
+            curr_val += int(line)
+        else:
+            if curr_val > max_kcal:
+                max_kcal = curr_val
+            curr_val = 0
+    print(f"day01 part1 answer is: {max_kcal}")
+
+
+def part2():
+    kcal_list = []
+    curr_val = 0;
+    for line in read_file(file_name):
+        line = line.strip()
+        if len(line) != 0:
+            curr_val += int(line)
+        else:
+            kcal_list.append(curr_val)
+            curr_val = 0
+    top3_sum = sum(sorted(kcal_list, reverse=True)[:3])
+    print(f"day01 part2 answer is: {top3_sum}")
 
 
 if __name__ == "__main__":
-    main()
+    part1()
+    part2()
