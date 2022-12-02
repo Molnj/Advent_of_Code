b274dc9ac28a8d9555170c923943a67c83daa6c0
Mate <momatehun@gmail.com>
2022-12-02 07:35:16 +0100

    day02

--- 2022/README.md
+++ 2022/README.md
@@ -7,3 +7,7 @@
 - **Day01** - Elf calories .\
 *Find the elf carrying the most calories*
 *Find the 3 elves carrying the most calories*
+
+- **Day02** - Rock Paper Scissors .\
+*Calculate resulting score of (enemy shape - player shape) pairs*
+*Calculate resulting score of (enemy shape - match outcome) pairs*
---
+++ 2022/Src/day02.py
@@ -0,0 +1,55 @@
+import os
+
+file_dir = os.path.dirname(os.path.realpath('__file__'))
+file_name = os.path.join(file_dir, '../txt_inputs/day02.txt')
+file_name = os.path.abspath(os.path.realpath(file_name))
+
+
+def read_file(file_path: str) -> str:
+    with open(file_path, "r+") as file:
+        contents = file.readlines()
+    return contents
+
+    
+def part1(txt: str) -> None:
+    point_cnt = 0
+    results = {    # points for outcome + points for shape
+        "A X": 3+1,
+        "A Y": 6+2,
+        "A Z": 0+3,
+        "B X": 0+1,
+        "B Y": 3+2,
+        "B Z": 6+3,
+        "C X": 6+1,
+        "C Y": 0+2,
+        "C Z": 3+3
+    }
+    for line in txt:
+        line = line.strip()
+        point_cnt += results[line]
+    print(f"day02 part1 answer is: {point_cnt}")
+
+
+def part2(txt: str) -> None:
+    point_cnt = 0
+    results = {    # points for outcome + points for shape
+        "A X": 0+3,
+        "A Y": 3+1,
+        "A Z": 6+2,
+        "B X": 0+1,
+        "B Y": 3+2,
+        "B Z": 6+3,
+        "C X": 0+2,
+        "C Y": 3+3,
+        "C Z": 6+1
+    }
+    for line in txt:
+        line = line.strip()
+        point_cnt += results[line]
+    print(f"day02 part2 answer is: {point_cnt}")
+
+
+if __name__ == "__main__":
+    txt_file = read_file(file_name)
+    part1(txt_file)
+    part2(txt_file)
