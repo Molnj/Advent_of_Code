fcd1b4521b226318f5b16777ac95335ed49f0c6a
Molnj <momatehun@gmail.com>
2025-12-02 07:13:52 +0100

    day_02

--- 2025/day_06/main.py
+++ 2025/day_06/main.py
@@ -1,17 +1,21 @@
-# https://adventofcode.com/2025/day/
+# https://adventofcode.com/2025/day/3
 
 import sys
 
-def process_file(path):
+def process_file(path: str):
     with open(path) as f:
         for line in f:
             pass
-    return 0
+    return 
 
-def main(path_input):
-    x = process_file(path_input)
-    print(f"Part 1: ")
-    print(f"Part 2: ")
+
+def solve(line: str, part: int = 1) -> :
+    return
+
+
+def main(path_input: str):
+    print(f"Part 1: {solve(path_input, 1)}")
+    print(f"Part 2: {solve(path_input, 2)}")
 
 
 if __name__ == "__main__":
